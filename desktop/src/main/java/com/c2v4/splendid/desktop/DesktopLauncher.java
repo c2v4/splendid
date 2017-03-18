3bfa4d27b4b91bfbca0d496a8fa752b05d255d6c
c2v4 <the_pro@wp.pl>
2017-03-18 16:36:07 +0100

    Fixed Building problem

--- desktop/src/main/java/com/c2v4/splendid/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/c2v4/splendid/desktop/DesktopLauncher.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.c2v4.splendid.SplendidGame;
 
-import static com.c2v4.splendid.SplendidGame.HEIGHT;
-import static com.c2v4.splendid.SplendidGame.WIDTH;
-
 /** Launches the desktop (LWJGL) application. */
 public class DesktopLauncher {
 
@@ -22,8 +19,8 @@ public class DesktopLauncher {
     private static LwjglApplicationConfiguration getDefaultConfiguration() {
         LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
         configuration.title = "Splendid";
-        configuration.width = Companion.getWIDTH();
-        configuration.height = Companion.getHEIGHT();
+        configuration.width = SplendidGame.Companion.getWIDTH();
+        configuration.height = SplendidGame.Companion.getHEIGHT();
         for (int size : new int[] { 128, 64, 32, 16 }) {
             configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
         }
